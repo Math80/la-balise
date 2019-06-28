fef1fc7dc91a8736088f4b90c6f3e60cf58e7495
Mathias T <mathiastujague@gmail.com>
2019-06-28 15:59:52 +0200

    débrouille

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
        <Switch>
        <Route exact path="/" component={Home} />
        <Route path="/video" component={Video} />
-       {/* <Route path="/quizz" component={Quizz} /> */}
+       <Route path="/quizz" component={Quizz} />
        <Route path="/resultats" component={Results} />
        </Switch>
        <Footer />
